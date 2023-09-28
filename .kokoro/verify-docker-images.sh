66c344d5426e5ad6e6c29b44b816179519cc7caa
Mridula Peddada <mpeddada@google.com>
2023-09-28 09:59:58 -0400

    add graalvm_b image to check

--- .kokoro/verify-docker-images.sh
+++ .kokoro/verify-docker-images.sh
@@ -4,7 +4,8 @@ JAVA_SHARED_CONFIG=1.5.7
 
 branchName=$(git name-rev $KOKORO_GIT_COMMIT | sed 's/.* //')
 if [[ "${branchName}" == *"release-please--branches--main"* ]]; then
-  gcloud container images describe gcr.io/mpeddada-test/graalvm_a:$JAVA_SHARED_CONFIG > /dev/null ; echo $?
+  gcloud container images describe gcr.io/loud-devrel-kokoro-resources/graalvm_a:$JAVA_SHARED_CONFIG > /dev/null ; echo $?
+  gcloud container images describe gcr.io/cloud-devrel-kokoro-resources/graalvm_b:$JAVA_SHARED_CONFIG > /dev/null ; echo $?
 RETURN_CODE=$?
 else
   echo "Skipping check for non-release branches"
