58f3a0d3594b44f244d090f4e51e734601c777a9
Mridula Peddada <mpeddada@google.com>
2023-10-02 13:42:26 -0400

    replace test project id; refactor imageNames list

--- .kokoro/verify-docker-images.sh
+++ .kokoro/verify-docker-images.sh
@@ -2,8 +2,20 @@
 
 # Script to verify the presence of GraalVM docker test images tagged with the latest java shared config version.
 
+function fetch_image_names() {
+  # Get the directory of the build script
+  scriptDir=$(realpath $(dirname "${BASH_SOURCE[0]}"))
+  # cd to the parent directory, i.e. the root of the git repo
+  cd ${scriptDir}/..
+
+  imageNames="$(grep "  - gcr.io/cloud-devrel-public-resources/" .cloudbuild/cloudbuild.yaml | cut -d "/" -f 3 | cut -d ":" -f 1)"
+  export imageNames
+  echo "$imageNames"
+}
+
 javaSharedConfigVersion=1.5.7 # {x-version-update:java-shared-config:current}
-imageNames=( "graalvm_a" "graalvm_b")
+imageNames=$(fetch_image_names)
+
 branchName=$(git name-rev "$KOKORO_GIT_COMMIT" | sed 's/.* //')
 gitCommitMessage=$(git log -1 "$(git rev-parse --short "$KOKORO_GIT_COMMIT")" | grep "chore(main): release *")
 
@@ -11,7 +23,7 @@ gitCommitMessage=$(git log -1 "$(git rev-parse --short "$KOKORO_GIT_COMMIT")" | 
 if [[ "${branchName}" == *"release-please--branches--main"* ]] && [[ ! $gitCommitMessage =~ "SNAPSHOT" ]]; then
 
   for name in "${imageNames[@]}"; do
-    fullContainerName="gcr.io/mpeddada-test/${name}:${javaSharedConfigVersion}"
+    fullContainerName="gcr.io/gcloud-devrel-public-resources/${name}:${javaSharedConfigVersion}"
     echo "Verifying presence of ${fullContainerName}"
     gcloud container images describe "${fullContainerName}" > /dev/null; exit_status=$?
     if [[ $exit_status = 0 ]]; then
