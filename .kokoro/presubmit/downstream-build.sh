6265521dbd353c8552a2f0254749db25d27a24a5
Mridula Peddada <mpeddada@google.com>
2023-11-15 16:16:53 -0500

    chore: add showcase tests to downstream kokoro check

--- .kokoro/presubmit/downstream-build.sh
+++ .kokoro/presubmit/downstream-build.sh
@@ -26,10 +26,10 @@ function modify_shared_config() {
 EOF
 }
 
-if [ -z "${MODULES_UNDER_TEST}" ]; then
-  echo "MODULES_UNDER_TEST must be set to run downstream-build.sh"
-  exit 1
-fi
+#if [ -z "${MODULES_UNDER_TEST}" ]; then
+#  echo "MODULES_UNDER_TEST must be set to run downstream-build.sh"
+#  exit 1
+#fi
 
 ### Round 1
 ## Get the directory of the build script and install java-shared-config
@@ -43,21 +43,49 @@ SHARED_CONFIG_VERSION=$(sed -e 's/xmlns=".*"//' pom.xml | xmllint --xpath '/proj
 mkdir -p "${HOME}/.m2"
 cp settings.xml "${HOME}/.m2"
 
-### Round 2
-# Update the shared-config version in google-cloud-jar-parent
-git clone "https://github.com/googleapis/google-cloud-java.git" --depth=1
-pushd google-cloud-java/google-cloud-pom-parent
+git clone "https://github.com/googleapis/sdk-platform-java" --depth=1
+
+## Update java-shared-config version
+pushd sdk-platform-java/showcase
 modify_shared_config
 popd
 
-### Round 3
-# Run the updated java-shared-config against google-cloud-java
-pushd google-cloud-java
-source ./.kokoro/common.sh
-RETURN_CODE=0
-setup_application_credentials
-setup_cloud "$MODULES_UNDER_TEST"
-run_graalvm_tests "$MODULES_UNDER_TEST"
+pushd sdk-platform-java
+mvn -B -ntp install --projects '!gapic-generator-java' -Dcheckstyle.skip -Dfmt.skip -DskipTests
+popd
+pushd sdk-platform-java/showcase/gapic-showcase
+SHOWCASE_VERSION=$(mvn help:evaluate -Dexpression=gapic-showcase.version -q -DforceStdout)
+popd
+
+## Start showcase server
+mkdir -p /usr/src/showcase
+curl --location https://github.com/googleapis/gapic-showcase/releases/download/v"${SHOWCASE_VERSION}"/gapic-showcase-"${SHOWCASE_VERSION}"-linux-amd64.tar.gz --output /usr/src/showcase/showcase-"${SHOWCASE_VERSION}"-linux-amd64.tar.gz
+pushd /usr/src/showcase/
+tar -xf showcase-*
+./gapic-showcase run &
+popd
+
+# Run showcase tests with `native` profile
+pushd sdk-platform-java/showcase
+mvn test -Pnative,-showcase -Denforcer.skip=true -ntp -B
+popd
+
+
+### Round 2
+# Update the shared-config version in google-cloud-jar-parent
+#git clone "https://github.com/googleapis/google-cloud-java.git" --depth=1
+#pushd google-cloud-java/google-cloud-pom-parent
+#modify_shared_config
+#popd
+#
+#### Round 3
+## Run the updated java-shared-config against google-cloud-java
+#pushd google-cloud-java
+#source ./.kokoro/common.sh
+#RETURN_CODE=0
+#setup_application_credentials
+#setup_cloud "$MODULES_UNDER_TEST"
+#run_graalvm_tests "$MODULES_UNDER_TEST"
 
 
 exit $RETURN_CODE
