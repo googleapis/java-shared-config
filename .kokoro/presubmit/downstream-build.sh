1cb148502d5de483577b033a11c0508fa8d2aa99
mpeddada1 <mpeddada@google.com>
2024-04-30 17:31:12 +0000

    fix structure test

--- .kokoro/presubmit/downstream-build.sh
+++ .kokoro/presubmit/downstream-build.sh
@@ -16,47 +16,6 @@
 set -eo pipefail
 set -x
 
-# In the given directory ($1),
-#   update the pom.xml's dependency on the given artifact ($2) to the given version ($3)
-# ex: update_dependency google-cloud-java/google-cloud-jar-parent google-cloud-shared-dependencies 1.2.3
-function update_pom_dependency {
-  pushd "$1" || exit 1
-  xmllint --shell pom.xml &>/dev/null <<EOF
-setns x=http://maven.apache.org/POM/4.0.0
-cd .//x:artifactId[text()="$2"]
-cd ../x:version
-set $3
-save pom.xml
-EOF
-  popd || exit 1
-}
-
-# Find all pom.xml files that declare a specific version for the given artifact ($1)
-function find_all_poms_with_versioned_dependency {
-  poms=($(find . -name pom.xml))
-  for pom in "${poms[@]}"; do
-    if xmllint --xpath "//*[local-name()='artifactId' and text()='$1']/following-sibling::*[local-name()='version']" "$pom" &>/dev/null; then
-      found+=("$pom")
-    fi
-  done
-  POMS=(${found[@]})
-  unset found
-  export POMS
-}
-
-# In the given directory ($1),
-#   find and update all pom.xmls' dependencies on the given artifact ($2) to the given version ($3)
-# ex: update_all_poms_dependency google-cloud-java google-cloud-shared-dependencies 1.2.3
-function update_all_poms_dependency {
-  pushd "$1" || exit 1
-  find_all_poms_with_versioned_dependency "$2"
-  for pom in $POMS; do
-    update_pom_dependency "$(dirname "$pom")" "$2" "$3"
-  done
-  git diff
-  popd || exit 1
-}
-
 function modify_shared_config() {
   xmllint --shell pom.xml <<EOF
   setns x=http://maven.apache.org/POM/4.0.0
@@ -106,21 +65,5 @@ popd
 pushd sdk-platform-java/showcase
 mvn test -Dtest='!com.google.showcase.v1beta1.it.ITHttpAnnotation' -Dsurefire.failIfNoSpecifiedTests=false -Pnative,-showcase -Denforcer.skip=true -ntp -B
 popd
-
-### Round 3
-# Update the shared-dependencies version in google-cloud-jar-parent
-git clone "https://github.com/googleapis/google-cloud-java.git" --depth=1
-update_all_poms_dependency google-cloud-java google-cloud-shared-dependencies "$SHARED_DEPS_VERSION"
-
-### Round 4
-# Run the updated java-shared-dependencies BOM against google-cloud-java integration tests
-cd google-cloud-java
-source ./.kokoro/common.sh
-RETURN_CODE=0
-setup_application_credentials
-setup_cloud "$MODULES_UNDER_TEST"
-run_graalvm_tests "$MODULES_UNDER_TEST"
-# Exit must occur in google-cloud-java directory to correctly destroy IT resources
-#exit "$RETURN_CODE"
 
 exit $RETURN_CODE
